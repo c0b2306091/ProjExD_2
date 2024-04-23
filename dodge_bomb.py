bb3ac976017b937fd47b0298fa981efc230ab69e
栗原潤 <jun@kuriharajunnoMacBook-Air.local>
2024-04-23 15:13:07 +0900

    練習3:はみ出しと爆弾完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,13 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct):
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -21,8 +28,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bg_rect.center
-    screen.built
+    bd_img = pg.Surface((20,20))
+    bd_img.set_colorkey((0,0,0))
+    pg.draw.circle(bd_img,(255,0,0),(10,10),10)
+    bd_rct=bd_img.get_rect()
+    bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx,vy = +5,+5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -37,6 +48,8 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         
         """ if key_lst[pg.K_UP]:
             sum_mv[1] -= 5
@@ -48,6 +61,15 @@ def main():
             sum_mv[0] += 5 """
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        bd_rct.move_ip(vx, vy)       
+        screen.blit(bd_img, bd_rct)
+        yoko,tate=check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
